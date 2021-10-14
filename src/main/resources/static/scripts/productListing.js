8471cf0642f7f6a42582f8210278911e686e9e61
maxxsmith <maxxsmith34@gmail.com>
2021-10-13 23:01:25 -0500

    Call player functionality, splash screen, file cleanup @maxx @eli

--- src/main/resources/static/scripts/productListing.js
+++ src/main/resources/static/scripts/productListing.js
@@ -1,24 +1,31 @@
-// var splash =  document.createElement("img");
-// splash.src = "https://github.com/webyteCSCE3513/photon-main/blob/main/logo.jpg?raw=true";
-// splash.width = screen.width / 2;
-// splash.height = screen.height / 2;
-// splash.onload 
-// console.log(splash.src);
-// var div = document.getElementById('x');
-// div.appendChild(splash);
-// setTimeout(() =>{ 
-// 	document.getElementById('splash').style.visibility='none';
-// }, 3000);
-
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", function(event) {
+	event.preventDefault(); //not sure what this does, really
 })
 
-function searchById(id){
+async function searchById(id, tableName){
 	var url = "http://localhost:8080/api/player/" + id;
-	fetch(url, {
+	const response = await fetch(url, {
 		mode:'no-cors'
 	})
-	.then(response => response.json())
- 	.then(data => console.log(data));
+	const player = await response.json(); //probably make it object
+	console.log(player);
+	console.log(typeof player);
+	var playerJSON = JSON.stringify(player);
+	console.log(typeof playerJSON);
+
+	playerJSON = JSON.parse(playerJSON);
+	console.log(playerJSON.id);
+	console.log(playerJSON.codename);
+
+	function addPlayer() {
+		var table = document.getElementById(tableName);
+		var row = table.insertRow();
+		var cell = row.insertCell();
+		cell.innerHTML = playerJSON.id;
+		cell = row.insertCell();
+		cell.innerHTML = playerJSON.codename;
+	}
+
+	addPlayer();
+
 }
